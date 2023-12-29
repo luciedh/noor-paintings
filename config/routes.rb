12f6cb0e889a8790207abe6f3f074b9e03611c5f
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-12-29 14:00:39 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
+  resources :artworks
+  get "the_last_sunbeam/artwork", to: "artworks#the_last_sunbeam", as: :the_last_sunbeam
+  get "linogravures/artwork", to: "artworks#linogravures", as: :linogravures
+  get "instantanes/artwork", to: "artworks#instantanes", as: :instantanes
+  get "poemes/artwork", to: "artworks#poemes", as: :poemes
 end
